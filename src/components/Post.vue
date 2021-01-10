a7e5005ed676f3c50515103e559e20beadcfaa6f
10011916 <10011916@ISTN35670>
2021-01-10 14:21:52 +0300

    changes

---
+++ src/components/Post.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="post">
+        <header class="post-header">
+            <div class="post-header-author">
+                <Avatar :size="40" />
+                <div class="author">
+                    <custom-text>Taha Zeybek</custom-text>
+                    <custom-text>İstanbul</custom-text>
+                </div>
+            </div>
+            <div class="post-header-more">
+                <img src="../icons/more.png" height="40" width="40" alt="" />
+            </div>
+        </header>
+        <div class="post-media">
+            <img
+                src="https://mediatrend.mediamarkt.com.tr/wp-content/uploads/2017/02/2017_subat_03.jpg"
+                alt=""
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import Avatar from '@/components/avatar'
+import CustomText from './CustomText'
+export default {
+    name: 'Post',
+    components: {
+        Avatar,
+        CustomText,
+    },
+}
+</script>
+CustomText
+
+<style lang="scss" scoped>
+.post {
+    img {
+        max-width: 100%;
+    }
+
+    .post-header {
+        display: flex;
+        align-items: center;
+        height: 60px;
+        &-author {
+            display: flex;
+            align-items: center;
+
+            .author {
+                display: flex;
+                flex-direction: column;
+                align-items: flex-start;
+            }
+        }
+        &-more {
+            margin-left: auto;
+        }
+    }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,86 +1,52 @@
 <template>
-  <div class="home">
-	  <div class="timeline">
-		  <h2>Home</h2> 
-		<CustomText>Componentlerrrr</CustomText>
-		<CustomText tag="p">Componentlerrrr</CustomText>
-		<CustomText tag="b" size="xsmall">Componentlerrrr</CustomText>
-		<img src="../../assets/logo.png" width="100" height="100" alt="">
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, cum corporis fugit ab non in iusto, exercitationem mollitia dolore at atque molestiae quod molestias eum deleniti maiores cupiditate consectetur. Obcaecati.
+    <div class="home">
+        <div class="timeline">
+            <h2>Home</h2>
 
-		</p>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, cum corporis fugit ab non in iusto, exercitationem mollitia dolore at atque molestiae quod molestias eum deleniti maiores cupiditate consectetur. Obcaecati.
-
-		</p>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, cum corporis fugit ab non in iusto, exercitationem mollitia dolore at atque molestiae quod molestias eum deleniti maiores cupiditate consectetur. Obcaecati.
-
-		</p>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, cum corporis fugit ab non in iusto, exercitationem mollitia dolore at atque molestiae quod molestias eum deleniti maiores cupiditate consectetur. Obcaecati.
-
-		</p>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, cum corporis fugit ab non in iusto, exercitationem mollitia dolore at atque molestiae quod molestias eum deleniti maiores cupiditate consectetur. Obcaecati.
-
-		</p>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, cum corporis fugit ab non in iusto, exercitationem mollitia dolore at atque molestiae quod molestias eum deleniti maiores cupiditate consectetur. Obcaecati.
-
-		</p>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, cum corporis fugit ab non in iusto, exercitationem mollitia dolore at atque molestiae quod molestias eum deleniti maiores cupiditate consectetur. Obcaecati.
-
-		</p>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, cum corporis fugit ab non in iusto, exercitationem mollitia dolore at atque molestiae quod molestias eum deleniti maiores cupiditate consectetur. Obcaecati.
-
-		</p>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, cum corporis fugit ab non in iusto, exercitationem mollitia dolore at atque molestiae quod molestias eum deleniti maiores cupiditate consectetur. Obcaecati.
-
-		</p>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, cum corporis fugit ab non in iusto, exercitationem mollitia dolore at atque molestiae quod molestias eum deleniti maiores cupiditate consectetur. Obcaecati.
-
-		</p>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, cum corporis fugit ab non in iusto, exercitationem mollitia dolore at atque molestiae quod molestias eum deleniti maiores cupiditate consectetur. Obcaecati.
-
-		</p>
-	  </div>
-	  <div class="sidebar">
-		  <p>
-			  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia numquam laboriosam dolorum expedita praesentium sint omnis autem. Unde blanditiis, omnis nesciunt quam voluptate voluptatem quod dolor optio quasi ducimus itaque!
-		  </p>
-	  </div>
-	
-  </div>
+            <Post />
+        </div>
+        <div class="sidebar">
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Mollitia numquam laboriosam dolorum expedita praesentium sint
+                omnis autem. Unde blanditiis, omnis nesciunt quam voluptate
+                voluptatem quod dolor optio quasi ducimus itaque!
+            </p>
+        </div>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import CustomText from "@/components/CustomText"
+import Post from '@/components/Post'
+
 export default {
-  name: "Home",
-  components: {
-	  CustomText,
-  }
-};
+    name: 'Home',
+    components: {
+        Post,
+    },
+}
 </script>
 
 <style lang="scss">
-.home{
-	
-	max-width: 605px;
-	margin-left: auto;
-	margin-right: auto;
+.home {
+    max-width: 605px;
+    margin-left: auto;
+    margin-right: auto;
 
-	@media screen and (min-width: 876px){
-		max-width: none;
-		display: grid;
-		grid-template-columns: 1fr 295px;
-		grid-gap: 30px;
-	}
+    @media screen and (min-width: 876px) {
+        max-width: none;
+        display: grid;
+        grid-template-columns: 1fr 295px;
+        grid-gap: 30px;
+    }
 
-	.sidebar{
-		display: none;
+    .sidebar {
+        display: none;
 
-		@media screen and (min-width: 876px) {
-			display: block;
-		}
-	}
-	
+        @media screen and (min-width: 876px) {
+            display: block;
+        }
+    }
 }
 </style>
